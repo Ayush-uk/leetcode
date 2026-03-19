ddc8bf65e8371ec6e452a52c45356212f0207304
ayush-uk <ayushukenday@gmail.com>
2026-03-19 11:56:25 +0530

    feat: integrate PostgreSQL with Prisma and update project configuration
    - Added Docker Compose configuration for PostgreSQL service.
    - Updated package.json to include Prisma and PostgreSQL dependencies.
    - Created db.js for Prisma client initialization.
    - Added Prisma configuration file (prisma.config.ts) for database connection.
    - Created initial migration for User model with appropriate fields and indexes.
    - Defined UserRole enum for user roles in the database schema.

---
+++ prisma/migrations/20260319062302_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "username" TEXT,
+    "email" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_role_idx" ON "User"("role");
